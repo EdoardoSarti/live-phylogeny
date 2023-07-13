131d29af39051996f96958718cd97086ec91d5b0
Mathevilg <109210548+Mathevilg@users.noreply.github.com>
2023-07-13 10:25:24 +0530

    Update nj.py

--- nj.py
+++ nj.py
@@ -2,6 +2,19 @@ from skbio import DistanceMatrix
 from skbio.tree import nj
 import networkx as nx
 
+def SI(a,b):
+    """returns SI value over non-gapped pairs"""
+    matches = 0
+    norm = 0
+    for i in range(len(a)):
+        if a[i] != '-' and b[i] != '-':
+            norm += 1
+            if a[i] == b[i]:
+                matches+=1
+    if norm == 0:
+        return 0.
+    return matches/norm
+
 def skbio_tree_to_nx_graph(tree):
     graph = nx.Graph()
 
@@ -9,18 +22,20 @@ def skbio_tree_to_nx_graph(tree):
         if node is None:
             return
 
-        # print("Node", node.name, node.children)
         counter = 0
         for child in node.children:
             if child.name is None:
                 child.name = node.name + '_' + str(counter)
                 counter += 1
             graph.add_node(child.name)
-            graph.add_edge(node.name, child.name, distance=round(node.distance(child),4))
+            d = node.distance(child)
+            # Change the threshold and corresponding weight here
+            if (d<=0.01) : graph.add_edge(node.name, child.name, weight=0.1)    
+            else : 
+                graph.add_edge(node.name, child.name, weight=d)
             traverse(child)
     tree.root().name = 'r'
     graph.add_node(tree.root().name)
     traverse(tree.root())
-    # print("TREE TO GRAPH:", graph)
 
     return graph
